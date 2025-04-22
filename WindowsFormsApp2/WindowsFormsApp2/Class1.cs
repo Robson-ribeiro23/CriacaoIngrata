b35ce13522bd1ba4cee69f7e1104243821b215ef
Vinícius Vieira Modesto <vinicius.modesto611@gmail.com>
2025-04-22 11:31:21 -0300

    initial

---
+++ WindowsFormsApp2/WindowsFormsApp2/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp2
+{
+    internal class Class1
+    {
+        List<int> ints = new List<int>();
+        List<string> strings = new List<string>();
+
+        public Class1() { 
+            ints.Add(0);
+            ints.Add(1);
+            ints.Add(2);
+        }
+    }
+}
